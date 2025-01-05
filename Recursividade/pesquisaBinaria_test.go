0c7a54575945ae56418de0a532ee9231bdcde468
SergioLNeves <sergioluiz852@gmail.com>
2025-01-05 20:03:17 -0300

    Adiciona Recursividade em PesquisaBinaria

---
+++ Recursividade/pesquisaBinaria_test.go
@@ -0,0 +1,29 @@
+package Recursividade
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_PesquisaBinaria(t *testing.T) {
+
+	t.Run("Should Return Number Ok", func(t *testing.T) {
+		numberExpected1 := 5
+
+		result1 := PesquisaBinaria(0, 50, numberExpected1)
+
+		assert.Equal(t, numberExpected1, result1)
+
+		numberExpected2 := 49
+
+		result2 := PesquisaBinaria(0, 50, numberExpected2)
+
+		assert.Equal(t, numberExpected2, result2)
+	})
+
+	t.Run("Should Return Error Number", func(t *testing.T) {
+		numberExpected := -1
+		number := PesquisaBinaria(0, 50, numberExpected)
+		assert.Equal(t, numberExpected, number)
+	})
+}
