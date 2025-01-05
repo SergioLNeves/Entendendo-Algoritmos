98cf5d7b99f66301a416d66f90b2a2955b94fcdc
SergioLNeves <sergioluiz852@gmail.com>
2025-01-05 18:17:05 -0300

    refact: refatora o código soma

--- Recursividade/soma.go
+++ Recursividade/soma.go
@@ -1,19 +1,9 @@
 package Recursividade
 
 func Soma(arr []int) int {
-	var soma int
-
 	if len(arr) == 0 {
 		return 0
 	}
 
-	for _, valor := range arr[1:] {
-		soma += valor
-	}
-
-	return arr[0] + soma
-}
-
-func ProcuraBase(arr []int) int {
-
+	return arr[0] + Soma(arr[1:])
 }
