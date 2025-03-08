b8e380df7f376778e06f19e9248a9cfcdb4547f0
SergioLNeves <sergioluiz852@gmail.com>
2025-03-08 12:47:35 -0300

    refactor: simplifica a inicialização da tabela hash

--- Hash/hash.go
+++ Hash/hash.go
@@ -3,11 +3,11 @@ package Hash
 // Hash - Mapeia a tabela hash e retorna o valor da key escolhida
 func Hash(key string) float64 {
 
-	tabela := map[string]float64{}
-
-	tabela["maçã"] = 2.99
-	tabela["leite"] = 4.99
-	tabela["abacate"] = 3.99
+	tabela := map[string]float64{
+		"maçã":    2.99,
+		"leite":   4.99,
+		"abacate": 3.99,
+	}
 
 	return tabela[key]
 }
